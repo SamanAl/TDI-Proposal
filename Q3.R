6be1e3182eb5bf03782b9e2cd6f8256026083bce
SamanAl <47951608+SamanAl@users.noreply.github.com>
2020-10-22 15:40:19 -0400

    Update Q3.R

--- Q3.R
+++ Q3.R
@@ -1,84 +1,102 @@
 
+#import DataFrame libraries
 library(dplyr)
 library(zoo)
+library(pointdensityP)
 tinytex:::is_tinytex()
 
 
-setwd('D:/Saman/TDI')
-
-lines = length(readLines("Data_NYPD.csv"))
-
-input=read.csv("Data_NYPD.csv", header = TRUE, fill = TRUE, sep=',')
-df = input[c(4:6,8:12,17:18)]; df$id = input
-
-dates = input$OCCUR_DATE
-dates = sapply(1:length(dates), 
-               function(i) as.numeric(unlist(strsplit(gsub(" 0:00","",dates[i]),'/'))))
-
-df$MONTH = dates[1,]; df$DAY = dates[2,]; df$YEAR = dates[3,]
-
-times = as.character(input$OCCUR_TIME); 
-times = sapply(1:length(times),function(i) as.numeric(unlist(strsplit(times[i],":"))))
-df$HOUR = times[1,]
-df$monyear <- as.yearmon(paste(df$YEAR, df$MONTH), "%Y %m")
-df$dates = as.Date(paste(dates[3,],'-',dates[1,],'-',dates[2,],sep=''))
-
-structured_df <- aggregate(STATISTICAL_MURDER_FLAG~BORO+monyear, as.data.frame(df),length)
-sample_of_data=df[sample(100,100),c(1,2,4,5,6,9,10,17)]
-colnames(sample_of_data) <- c("Borough","Precinct","Murder","Age Group","Sex","Lat","Lon","Dates")
-
+#import visualization libraries
 library(reshape2)
 library(gridExtra)
 library(grid)
 library(ggplot2)
 library(lattice)
 
-df_plot <- structured_df[which(structured_df[,1]=='BROOKLYN'),]
-df_plot <- melt(df_plot[, c("monyear", "STATISTICAL_MURDER_FLAG")], id="monyear")
-p1=ggplot(df_plot) + geom_line(aes(x=monyear, y=value),color='black',size = 1.2)+ 
+#set working directory
+setwd('~/Saman/TDI')
+
+
+#import gun violence data
+lines = length(readLines("Data_NYPD.csv"))
+input=read.csv("Data_NYPD.csv", header = TRUE, fill = TRUE, sep=',')
+nyc_gun_data= input[c(4:6,8:12,17:18)]; nyc_gun_data$id = input
+
+
+#import weather data
+weather_data=read.csv('weather.csv')
+weather_dates=as.numeric(as.Date(weather_data$DATE))
+
+#Structuring the dates
+dates = input$OCCUR_DATE
+dates = sapply(1:length(dates), 
+               function(i) as.numeric(unlist(strsplit(gsub(" 0:00","",dates[i]),'/'))))
+
+nyc_gun_data$MONTH = dates[1,]; nyc_gun_data$DAY = dates[2,]; nyc_gun_data$YEAR = dates[3,]
+
+times = as.character(input$OCCUR_TIME); 
+times = sapply(1:length(times),function(i) as.numeric(unlist(strsplit(times[i],":"))))
+nyc_gun_data$HOUR = times[1,]
+nyc_gun_data$monyear <- as.yearmon(paste(nyc_gun_data$YEAR, nyc_gun_data$MONTH), "%Y %m")
+nyc_gun_data$dates = as.Date(paste(dates[3,],'-',dates[1,],'-',dates[2,],sep=''))
+
+
+#insights from data aggregation
+structured_nyc_gun_data<- aggregate(STATISTICAL_MURDER_FLAG~BORO+monyear, as.data.frame(nyc_gun_data),length)
+sample_of_data=nyc_gun_data[sample(100,100),c(1,2,4,5,6,9,10,17)]
+colnames(sample_of_data) <- c("Borough","Precinct","Murder","Age Group","Sex","Lat","Lon","Dates")
+
+
+# EDA plots on the general trends in each location
+nyc_gun_data_plot <- structured_nyc_gun_data[which(structured_nyc_gun_data[,1]=='BROOKLYN'),]
+nyc_gun_data_plot <- melt(nyc_gun_data_plot[, c("monyear", "STATISTICAL_MURDER_FLAG")], id="monyear")
+p1=ggplot(nyc_gun_data_plot) + geom_line(aes(x=monyear, y=value),color='black',size = 1.2)+ 
   stat_smooth(aes(x = monyear, y = value),color='red', method = "lm",
               formula = y ~ poly(x, 1), se = TRUE)+ ggtitle("BROOKLYN")+
-  labs(x = "Date", y='Shooting')
+		labs(x = "Date", y='Shooting')
 
-df_plot <- structured_df[which(structured_df[,1]=='BRONX'),]
-df_plot <- melt(df_plot[, c("monyear", "STATISTICAL_MURDER_FLAG")], id="monyear")
+nyc_gun_data_plot <- structured_nyc_gun_data[which(structured_nyc_gun_data[,1]=='BRONX'),]
+nyc_gun_data_plot <- melt(nyc_gun_data_plot[, c("monyear", "STATISTICAL_MURDER_FLAG")], id="monyear")
 
-p2=ggplot(df_plot) + geom_line(aes(x=monyear, y=value),color='black',size = 1.2)+ 
+
+p2=ggplot(nyc_gun_data_plot) + geom_line(aes(x=monyear, y=value),color='black',size = 1.2)+ 
   stat_smooth(aes(x = monyear, y = value),color='red', method = "lm",
               formula = y ~ poly(x, 1), se = TRUE)+ ggtitle("BRONX")+
   labs(x = "Date", y='Shooting')
 
-df_plot <- structured_df[which(structured_df[,1]=='MANHATTAN'),]
-df_plot <- melt(df_plot[, c("monyear", "STATISTICAL_MURDER_FLAG")], id="monyear")
+nyc_gun_data_plot <- structured_nyc_gun_data[which(structured_nyc_gun_data[,1]=='MANHATTAN'),]
+nyc_gun_data_plot <- melt(nyc_gun_data_plot[, c("monyear", "STATISTICAL_MURDER_FLAG")], id="monyear")
 
-p3=ggplot(df_plot) + geom_line(aes(x=monyear, y=value),color='black',size = 1.2)+ 
+
+p3=ggplot(nyc_gun_data_plot) + geom_line(aes(x=monyear, y=value),color='black',size = 1.2)+ 
   stat_smooth(aes(x = monyear, y = value),color='red', method = "lm",
               formula = y ~ poly(x, 1), se = TRUE)+ ggtitle("MANHATTAN")+
   labs(x = "Date", y='Shooting')
 
-df_plot <- structured_df[which(structured_df[,1]=='QUEENS'),]
-df_plot <- melt(df_plot[, c("monyear", "STATISTICAL_MURDER_FLAG")], id="monyear")
+nyc_gun_data_plot <- structured_nyc_gun_data[which(structured_nyc_gun_data[,1]=='QUEENS'),]
+nyc_gun_data_plot <- melt(nyc_gun_data_plot[, c("monyear", "STATISTICAL_MURDER_FLAG")], id="monyear")
 
-p4=ggplot(df_plot) + geom_line(aes(x=monyear, y=value),color='black',size = 1.2)+ 
+
+p4=ggplot(nyc_gun_data_plot) + geom_line(aes(x=monyear, y=value),color='black',size = 1.2)+ 
   stat_smooth(aes(x = monyear, y = value),color='red', method = "lm",
               formula = y ~ poly(x, 1), se = TRUE)+ ggtitle("QUEENS")+
   labs(x = "Date", y='Shooting')
 
 grid.arrange(p1, p2, p3, p4, ncol = 2)
 
-df_plot <- aggregate(STATISTICAL_MURDER_FLAG~MONTH,as.data.frame(df),length)
-barplot(sapply(1:12,function(x) length(df$STATISTICAL_MURDER_FLAG[which(df$MONTH==x)])),
+
+# EDA plots to visualize seasonality 
+nyc_gun_data_plot <- aggregate(STATISTICAL_MURDER_FLAG~MONTH,as.data.frame(nyc_gun_data),length)
+barplot(sapply(1:12,function(x) length(nyc_gun_data$STATISTICAL_MURDER_FLAG[which(nyc_gun_data$MONTH==x)])),
         col=rainbow(12),names=month.abb[1:12],ylab='Number of Shootings',
         font=2,cex.axis = 0.7,cex.names=0.7,cex.lab=0.8)
 
 
-weather_data=read.csv('D:/Saman/TDI/weather.csv')
-weather_dates=as.numeric(as.Date(weather_data$DATE))
 
-structured_df <- aggregate(STATISTICAL_MURDER_FLAG~dates, as.data.frame(df),length)
-id_dates_shooting=as.numeric(as.Date(structured_df$dates))
+structured_nyc_gun_data<- aggregate(STATISTICAL_MURDER_FLAG~dates, as.data.frame(nyc_gun_data),length)
+id_dates_shooting=as.numeric(as.Date(structured_nyc_gun_data$dates))
 
-id_dates=as.numeric(as.Date(df$dates))
+id_dates=as.numeric(as.Date(nyc_gun_data$dates))
 
 ranks=apply(weather_data[,c(4,7:9)],2,function(x) round(rank(x)/length(x),2))
 
@@ -86,11 +104,11 @@ weather_shooting=sapply(1:length(id_dates_shooting),
                         function(x) ranks[which(weather_dates==id_dates_shooting[x]),])
 
 
-thresh=quantile(structured_df$STATISTICAL_MURDER_FLAG,seq(0,1,0.2))
+thresh=quantile(structured_nyc_gun_data$STATISTICAL_MURDER_FLAG,seq(0,1,0.2))
 thresh=as.numeric(thresh)+0.5
 labels_ins <- c('0-0.2','0.2-0.4','0.4-0.6','0.6-0.8','0.8-1')
 
-classified_shootings=cut(structured_df$STATISTICAL_MURDER_FLAG,
+classified_shootings=cut(structured_nyc_gun_data$STATISTICAL_MURDER_FLAG,
                          breaks = as.numeric(thresh), 
                          labels = as.character(1:(length(thresh)-1)))
 
@@ -112,33 +130,29 @@ boxplot(sapply(1:(length(thresh)-1), function(x) weather_shooting[1,which(classi
         font=2,cex.axis = 0.7,cex.names=0.7,cex.lab=0.8)
 
 
-library(pointdensityP)
-library(ggmap)
 
-register_google(key = "")
+#EDA plots for Spatial kernel density
+register_google(key = OS.ENV.KEY)
 has_google_key()
 
+#Show the density map using KDE
 density_shot = NULL
 
 for (i in 1:12){
   
   yr_ind = 2006:2018
   
-  dp=pointdensity(df[which(df$YEAR==yr_ind[i]),], lat_col="Latitude", lon_col="Longitude",date_col = "dates", 1, 2)
+  dp=pointdensity(nyc_gun_data[which(nyc_gun_data$YEAR==yr_ind[i]),], lat_col="Latitude", lon_col="Longitude",date_col = "dates", 1, 2)
   density_shot = c(density_shot,max(dp$count))
   nyc_map <- get_map(location = c(lon = -73.95, lat = 40.73), maptype = "terrain", zoom = 11)
   p=ggmap(nyc_map)
-  p=p + geom_point(data=dp[c((nrow(dp)-40),nrow(dp)),], aes(x=lon, y=lat),size=8,color='darkred')+ theme(axis.text=element_text(size=6),
-                                                                                                         axis.title=element_text(size=10,face="bold"))+ggtitle(yr_ind[i])
+  p=p + geom_point(data=dp[c((nrow(dp)-40),nrow(dp)),], 
+	aes(x=lon, y=lat),size=8,color='darkred')+ 
+	theme(axis.text=element_text(size=6),
+	axis.title=element_text(size=10,face="bold"))+ggtitle(yr_ind[i])
+
   assign(paste("P", i, sep = ""), p)  
 }
 
 pcomb=grid.arrange(P3, P6, P9, P12, ncol = 2)
-
-
-df_plot = list(); df_plot$Density = density_shot; df_plot$Year=c(2005.5:2016.5)
-df_plot = as.data.frame(df_plot)
-ggplot(df_plot) + geom_line(aes(x=Year, y=Density),color='black',size = 1.2)+ggtitle("Density")+scale_x_continuous(labels=scaleFUN)
-
-
 
